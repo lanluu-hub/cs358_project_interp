453382a1c1adf4cdbf7f400fdcb004d51c5f0a4d
Lan Quy Luu <luuquylan.keil@gmail.com>
2026-04-21 07:24:34 -0700

    feat: added eval, evalInEnv for numeric ast

--- project/interp.py
+++ project/interp.py
@@ -43,6 +43,7 @@ class Neg():
     subexpr: Expr
     def __str__(self) -> str:
         return f"(- {self.subexpr})"
+    
 @dataclass
 class Let():
     name: str
@@ -63,14 +64,106 @@ class Lit():
     def __str__(self) -> str:
         return f"{self.value}"
     
+# Eval
+type Binding[V] = tuple[str,V] # this tuple type is always a pair
+type Env[V] = tuple[Binding[V], ...] # This tuple type has arbitrary length
+
+from typing import Any
+emptyEnv : Env[Any] = () # the empty enviroment has no bindings
+
+def extendEnv[V](name: str, value: V, env:Env[V]) -> Env[V]:
+    '''Return a new environment that extends the input environment env with a new binding from name to value'''
+    return ((name,value),) + env
+    
+class EnvError(Exception):
+    pass
+
+def lookupEnv[V](name: str, env: Env[V]) -> V :
+    '''Return the first value bound to name in the input environment env
+       (or raise an exception if there is no such binding)'''
+    try:
+        return next(v for (n,v) in env if n == name)   # use handy generator expression to search for name
+    except StopIteration:
+        raise EnvError('name is not in environment: ' + name)        
+
+"""
+# Alternative (simplier)
+def lookupEnv[V](name: str, env: Env[V]) -> (V | None):
+    '''Return the first value bound to name in the input environment env
+       (or raise an exception if there is no such binding)'''
+    match env:
+        case ((n,v), *rest) :
+            if n == name:
+                return v
+            else:
+                return lookupEnv(name, rest) # type:ignore
+        case _:
+            return None
+"""
+
+class EvalError(Exception):
+    pass
+
+def eval(e: Expr) -> Literal:
+    return evalInEnv(emptyEnv, e)
+
+def evalInEnv(env: Env[Literal], e: Expr) -> Literal:
+    match e:
+        case Add(l,r):
+            match (evalInEnv(env, l), evalInEnv(env,r)):
+                case (int(lv), int(rv)):
+                    return lv + rv
+                case _:
+                    raise EvalError("addition of non-intergers")
+                
+            match Sub(l,r):
+                case (int(lv), int(rv)):
+                    return lv - rv
+                case _:
+                    raise EvalError("subtraction of non-integers")
+        case Mul(l,r):
+            match (evalInEnv(env, l), evalInEnv(env,r)):
+                case (int(lv), int(rv)):
+                    return lv * rv
+                case _:
+                    raise EvalError("multiplication of non-integers")
+        case Div(l,r):
+            match (evalInEnv(env, l), evalInEnv(env,r)):
+                case (int(lv), int(rv)):
+                    if rv == 0:
+                        raise EvalError("division by zero")
+                    return lv // rv
+                case _:
+                    raise EvalError("division of non-integers")
+        case Neg(s):
+            match evalInEnv(env,s):
+                case int(i):
+                    return -i
+                case _:
+                    raise EvalError("negation of non-integer")
+        case Lit(lit):
+            match lit: #n-level matching keeps type-checking happy
+                case int(i):
+                    return i
+                #TODO: add for Bool and Str later
+        case Name(n):
+            v = lookupEnv(n, env)
+            if v is None:
+                raise EvalError(f"unbound name {n}")
+            return v
+        case Let(n,d,b):
+            v = evalInEnv(env, d)
+            newEnv = extendEnv(n, v, env)
+            return evalInEnv(newEnv, b)
+
 def main():
     a : Expr = Add(Lit(1), Lit(1))
     b : Expr = Mul(Lit(2), Add(Lit(1), Neg(Lit(1))))
     c : Expr = Let('x', Lit(2), Div(Lit(4), Name('x')))
 
-    print(a)
-    print(b)
-    print(c)
+    print(f"{a} = {eval(a)}")
+    print(f"{b} = {eval(b)}")
+    print(f"{c} = {eval(c)}")
 
 if __name__=="__main__":
     main()
