aa0ecd26ed29ea622909d6ec7da7d1f995a840d3
Lan Quy Luu <luuquylan.keil@gmail.com>
2026-04-21 06:48:50 -0700

    feat: added Let, Name, main func for testing

--- project/interp.py
+++ project/interp.py
@@ -1,10 +1,14 @@
 #TODO: Add header comment
+#TODO: Add Bool ast Expr
+#TODO: Add Let, Name
+#TODO: Add Eval, EvalInEnv
+
 
 from dataclasses import dataclass
 
 type Literal = int
 
-type Expr = Add | Sub | Mul | Div | Neg | Lit
+type Expr = Add | Sub | Mul | Div | Neg | Let | Name | Lit
 
 @dataclass
 class Add():
@@ -39,6 +43,19 @@ class Neg():
     subexpr: Expr
     def __str__(self) -> str:
         return f"(- {self.subexpr})"
+@dataclass
+class Let():
+    name: str
+    defexpr: Expr
+    bodyexpr: Expr
+    def __str__(self) -> str:
+        return f"(let {self.name} = {self.defexpr} in {self.bodyexpr})" 
+    
+@dataclass
+class Name():
+    name: str
+    def __str__(self) -> str:
+        return self.name
     
 @dataclass
 class Lit():
@@ -46,3 +63,14 @@ class Lit():
     def __str__(self) -> str:
         return f"{self.value}"
     
+def main():
+    a : Expr = Add(Lit(1), Lit(1))
+    b : Expr = Mul(Lit(2), Add(Lit(1), Neg(Lit(1))))
+    c : Expr = Let('x', Lit(2), Div(Lit(4), Name('x')))
+
+    print(a)
+    print(b)
+    print(c)
+
+if __name__=="__main__":
+    main()
